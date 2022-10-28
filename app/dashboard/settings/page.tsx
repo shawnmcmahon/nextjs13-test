7c7d993cbe78cefc84cf6ece4551d74b7f6cc1e5
shawnmcmahon <shawnmcmahon17@gmail.com>
2022-10-27 20:18:44 -0600

    Add dashboard/settings route

--- app/dashboard/settings/page.tsx
+++ app/dashboard/settings/page.tsx
@@ -1,12 +1,12 @@
 import Image from 'next/image'
-import styles from './page.module.css'
+import styles from '../../page.module.css'
 
-export default function Home() {
+export default function Settings() {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Hello World
+          Settings
         </h1>
       </main>
     </div>
