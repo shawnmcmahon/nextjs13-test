f51ea3961235cbcd8d2bcb0015843603f7d703cb
shawnmcmahon <shawnmcmahon17@gmail.com>
2022-10-30 12:28:19 -0600

    Add profile name

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,14 @@ import styles from './page.module.css'
 
 export default async function Home() {
 
-  const gitHubProfile = await fetch('https://api.github.com/users/shawnmcmahon');
+  const gitHubProfile = await (await fetch('https://api.github.com/users/shawnmcmahon')).json();
   console.log(gitHubProfile);
 
   return (
     <div className={styles.container}>
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Hello World
+          {gitHubProfile.name}
         </h1>
       </main>
     </div>
