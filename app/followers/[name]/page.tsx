cb6b41c4c40409dd4f99e64d61a00ef2a765b127
shawnmcmahon <shawnmcmahon17@gmail.com>
2022-10-30 13:42:31 -0600

    Fix error due to lack of layout.tsx

---
+++ app/followers/[name]/page.tsx
@@ -0,0 +1,19 @@
+import styles from './page.module.css'
+import { Counter } from '../../../components/test'
+
+export default async function Home({ params }: { params: { name: string } }) {
+  const name = "shawnmcmahon"
+  const gitHubProfile = await (await fetch('https://api.github.com/users/' + params.name)).json();
+  console.log(gitHubProfile);
+
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          {gitHubProfile.name} has {gitHubProfile.followers} followers
+        </h1>
+        <Counter />
+      </main>
+    </div>
+  )
+}
